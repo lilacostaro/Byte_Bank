b0496f47ce0933f2b33b15b0bca051a11254bdae
lilacostaro <lila.costa.ro@gmail.com>
2022-11-01 20:44:05 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.Modelos/ClasseTeste.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    class ClasseTeste
+    {
+        public ClasseTeste()
+        {
+            ModificadoresTeste teste = new ModificadoresTeste();
+            teste.MetodoPublico();
+            //teste.MetodoPrivado();
+            //teste.MetodoProtegido();
+            teste.MetodoInterno();
+        }
+        
+    }
+
+    class ClasseDerivada : ModificadoresTeste
+    {
+        public ClasseDerivada()
+        {
+            MetodoProtegido();
+        }
+    }
+
+    public class ModificadoresTeste
+    {
+        public void MetodoPublico()
+        {
+            // visivel fora da classe "ModificadoresTeste"
+        }
+
+        private void MetodoPrivado()
+        {
+            // visivel apenas na classe "ModificadoresTeste"
+        }
+
+        protected void MetodoProtegido()
+        {
+            // visivel apenas na classe "ModificadoresTeste" e derivados
+        }
+
+        internal void MetodoInterno()
+        {
+            // visivel apenas para o projeto ByteBank.Modelos!
+        }
+    }
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+//using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using Humanizer;
+
+namespace Bytebank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            Console.WriteLine(conta.Numero);
+
+            new ContaCorrente(458, 58987);
+
+            DateTime dataFimPagamento = new DateTime(2023, 12, 15);
+
+            DateTime dataCorente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimPagamento - dataCorente;
+
+            Console.WriteLine(dataCorente);
+            Console.WriteLine(dataFimPagamento);
+
+            string mensagem = $"Vencimento em {TimeSpanHumanizeExtensions.Humanize(diferenca)}";
+            Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+        }
+    }
+}
+        
